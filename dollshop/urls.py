b7571d8891b43722db71a9e0d58004f8f5f48af4
Dmitry Andreev <bossfirot@gmail.com>
2022-07-23 14:31:20 +0300

    add homepage and optimize

--- dollshop/urls.py
+++ dollshop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from dollshop.views import homepage
 
-app_name = 'homepage'
+app_name = 'dollshop'
 
 urlpatterns = [
     path('', homepage, name='homepage')
--- dollshop/views.py
+++ dollshop/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from dollshop.models import Category, Product
 
 
 def homepage(request):
-    return render(request, 'dollshop/index.html')
+    category = Category.objects.all()
+    product = Product.objects.all()
+    context ={
+        'category': category,
+        'product': product,
+    }
+    return render(request, 'dollshop/index.html', context)
