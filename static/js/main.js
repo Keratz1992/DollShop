cf0fcf72a94194ebe0a4fa460ab23c1eb3de1c71
Maxim Andreev <boss.skazka@mail.ru>
2022-07-23 13:54:31 +0300

    add files

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        dots: false,
+        nav: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        smartSpeed: 1500,
+        autoplayHoverPause: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.nav__link').click(function (e) {
+    e.preventDefault();
+    $('.nav__link').removeClass('nav__link--active');
+    $(this).addClass('nav__link--active')
+  })
+});
+$(document).ready(function () {
+  $('.menu__item').click(function (e) {
+    e.preventDefault();
+    $('.menu__item').removeClass('menu__item--active');
+    $(this).addClass('menu__item--active')
+  })
+});
